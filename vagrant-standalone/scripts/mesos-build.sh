26c3d768437f4bbef4292bd7cc455b2a1ab0aac6
Shingo Omura <everpeace@gmail.com>
2013-07-26 01:29:47 -0700

    update mesos-build script

--- vagrant-standalone/scripts/mesos-build.sh
+++ vagrant-standalone/scripts/mesos-build.sh
@@ -1,11 +1,25 @@
+function prompt(){
+  echo "$1?"
+  select ans in Yes No
+  do
+    case $ans in
+      Yes)
+        $1
+        ;;
+      No)
+        ;;
+    esac
+    break;
+  done
+}
+
 cd ~/
-git clone https://github.com/apache/incubator-mesos.git
-cd incubator-mesos
+git clone https://github.com/apache/mesos.git
+cd mesos
 ./bootstrap
 mkdir build
 cd build
-../configure
-make
-sudo make check
-sudo make install
-
+prompt "../configure --with-webui"
+prompt "make"
+prompt "sudo make check"
+prompt "sudo make install"
